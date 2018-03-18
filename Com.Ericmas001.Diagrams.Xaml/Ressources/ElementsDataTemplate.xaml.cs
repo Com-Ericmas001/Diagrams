cc97878d974cc5fadb887b36a4ee0d473f5c8cb1
Éric Massé <Ericmas001@gmail.com>
2018-03-17 20:32:45 -0400

    0.0.3 Better RessourceDictionnary Export

---
+++ Com.Ericmas001.Diagrams.Xaml/Ressources/ElementsDataTemplate.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Com.Ericmas001.Diagrams.Xaml.Ressources
+{
+    public partial class ElementsDataTemplate
+    {
+        //Expose it as singleton to avoid multiple instances of this dictionary
+        public static ElementsDataTemplate Instance { get; } = new ElementsDataTemplate();
+
+        public ElementsDataTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
